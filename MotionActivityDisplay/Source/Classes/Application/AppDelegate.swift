214a8093eee79e455b7a1471f604f705f2906cc4
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-18 23:47:39 +0200

    Add Xcode project

---
+++ MotionActivityDisplay/Source/Classes/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MotionActivityDisplay
+//
+//  Created by Julian Grosshauser on 18/06/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MotionActivityDisplayTests/Sources/Classes/MotionActivityDisplayTests.swift
@@ -0,0 +1,21 @@
+//
+//  MotionActivityDisplayTests.swift
+//  MotionActivityDisplayTests
+//
+//  Created by Julian Grosshauser on 18/06/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MotionActivityDisplayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
