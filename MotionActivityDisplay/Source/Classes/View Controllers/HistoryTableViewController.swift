efbc10f233f2e979733a7598241c7904669e5fcd
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-19 23:00:06 +0200

    Add HistoryTableViewController

---
+++ MotionActivityDisplay/Source/Classes/View Controllers/HistoryTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryTableViewController.swift
+//  MotionActivityDisplay
+//
+//  Created by Julian Grosshauser on 19/06/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewController: UITableViewController {
+
+}
