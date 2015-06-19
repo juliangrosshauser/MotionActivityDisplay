8a853832987a62bd5a705879331bcdd6c3cfd3fc
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-19 22:38:16 +0200

    Add MotionActivityViewController

---
+++ MotionActivityDisplay/Source/Classes/View Controllers/MotionActivityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MotionActivityViewController.swift
+//  MotionActivityDisplay
+//
+//  Created by Julian Grosshauser on 19/06/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class MotionActivityViewController: UIViewController {
+
+}
